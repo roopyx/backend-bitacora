16f63a331213523179a8477a4baf3a96a5ddca71
roopyx <roopyx.1111@gmail.com>
2025-04-29 19:04:53 -0400

    initial project

---
+++ src/main/java/com/lavv/springboot/backendbitacora/BackendBitacoraApplication.java
@@ -0,0 +1,13 @@
+package com.lavv.springboot.backendbitacora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBitacoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBitacoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lavv/springboot/backendbitacora/BackendBitacoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavv.springboot.backendbitacora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBitacoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
